57ea95cc2f20cd4a886ed054bee2b52388050547
Macaskill, David <david.macaskill@dieboldnixdorf.com>
2022-09-22 09:19:06 -0400

    Initial version of hockeyhero generated by generator-jhipster@7.9.3

---
+++ src/test/java/com/hockeyhero/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.hockeyhero;
+
+import com.hockeyhero.HockeyheroApp;
+import com.hockeyhero.config.AsyncSyncConfiguration;
+import com.hockeyhero.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { HockeyheroApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
